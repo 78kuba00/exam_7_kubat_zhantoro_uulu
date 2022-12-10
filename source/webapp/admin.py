188a52a6d196b9fa76978e162dcd1e699f6e3e09
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-12-10 12:53:30 +0600

    Step 1 complete: Made two models and registered them in admin

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from webapp.models import Poll, Choice
 
-# Register your models here.
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'created_at']
+    exclude = []
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'variation', 'poll']
+    exclude = []
+
+admin.site.register(Poll)
+admin.site.register(Choice)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+class Poll(models.Model):
+    question = models.CharField(max_length=100, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return f'{self.pk}. {self.question}'
 # Create your models here.
+class Choice(models.Model):
+    variation = models.CharField(max_length=500, verbose_name='Текст Варианта')
+    poll = models.ForeignKey('webapp.Poll', related_name='choices', on_delete=models.CASCADE, verbose_name='Опрос')
+
+    def __str__(self):
+        return f'{self.pk}. {self.variation}'
